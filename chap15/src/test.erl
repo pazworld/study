58b915be401299be41dfb0362311899467b6102e
pazworld <paz@moudamepo.net>
2023-11-06 20:39:06 +0900

    add test for event

--- chap15/src/test.erl
+++ chap15/src/test.erl
@@ -4,14 +4,32 @@
 % execute: erl -pa ebin
 
 -module(test).
--export([test1/0]).
+-export([test1/0, test2/0, test3/0]).
 -record(state, {server,
                 name="",
                 to_go=0}).
 
 test1() ->
-    io:format("event set, wait 5 seconds.~n"),
+    io:format("event set after 5 seconds, wait.~n"),
     spawn(event, loop, [#state{server=self(), name="test", to_go=[5]}]),
     receive
-        X -> io:format("event received:~p~n", [X])
+        X -> io:format("event received: ~p~n", [X])
+    end.
+
+test2() ->
+    io:format("event set after 500 seconds.~n"),
+    Pid = event:start("Event", 500),
+    timer:sleep(1000),
+    io:format("event cancel.~n"),
+    event:cancel(Pid).
+
+test3() ->
+    Now = erlang:localtime(),
+    DateInSeconds = calendar:datetime_to_gregorian_seconds(Now),
+    NewDateInSeconds = DateInSeconds + 3,
+    SecondsAfter = calendar:gregorian_seconds_to_datetime(NewDateInSeconds),
+    io:format("event set with start/2, wait 3 seconds.~n"),
+    event:start("Event", SecondsAfter),
+    receive
+        X -> io:format("event received: ~p~n", [X])
     end.
