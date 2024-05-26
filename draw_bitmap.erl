6ea4bb576c959709957878cae5a862c355fe403f
pazworld <paz@moudamepo.net>
2024-05-26 21:28:41 +0900

    add second logo in black background with heart

--- draw_bitmap.erl
+++ draw_bitmap.erl
@@ -14,4 +14,21 @@ start() ->
     Logo = wxBitmap:new(Image),
     CDC = wxClientDC:new(Frame),
     wxDC:drawBitmap(CDC, Logo, {10,10}),
+
+    Bitmap = wxBitmap:new(wxBitmap:getWidth(Logo)+40, wxBitmap:getHeight(Logo)+40),
+    MDC = wxMemoryDC:new(Bitmap),
+    wxDC:setBackground(MDC, wxBrush:new({10,10,10})),
+    wxDC:clear(MDC),
+    wxDC:drawBitmap(MDC, Logo, {20,20}),
+
+    Heart = fun(DC, X, Y) ->
+        wxDC:drawLines(DC, [{X,Y},{X-20,Y+30}, {X-40,Y}]),
+        wxDC:setBrush(DC, wxBrush:new({255,0,0})),
+        wxDC:drawArc(DC, {X,Y}, {X-20,Y}, {X-10,Y}),
+        wxDC:drawArc(DC, {X-20,Y}, {X-40,Y}, {X-30,Y}),
+        wxDC:floodFill(DC, {X-20, Y+10}, {0,0,0}, [{style, 2}])
+    end,
+    Heart(MDC, wxBitmap:getWidth(Bitmap) - 5, 15),
+    wxMemoryDC:destroy(MDC),
+    wxDC:drawBitmap(CDC, Bitmap, {150,10}),
     ok.
