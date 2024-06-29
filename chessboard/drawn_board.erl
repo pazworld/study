098c56f85fc6141a9fee68861123eae85c5212db
pazworld <paz@moudamepo.net>
2024-06-29 10:53:39 +0900

    change vertical location of white and black

--- chessboard/drawn_board.erl
+++ chessboard/drawn_board.erl
@@ -100,10 +100,10 @@ init_board() ->
                    {black, king}, {black, bishop}, {black, knight}, {black, rook}],
     WhitePieces = [{white, rook}, {white, knight}, {white, bishop}, {white, queen},
                    {white, king}, {white, bishop}, {white, knight}, {white, rook}],
-    Row1 = [{{C, 1}, {white, pawn}} || C <- Columns ],
-    Row6 = [{{C, 6}, {black, pawn}} || C <- Columns ],
-    Row0 = [{{C, 0}, lists:nth(C + 1, WhitePieces)} || C <- Columns ],
-    Row7 = [{{C, 7}, lists:nth(C + 1, BlackPieces)} || C <- Columns ],
+    Row1 = [{{C, 1}, {black, pawn}} || C <- Columns ],
+    Row6 = [{{C, 6}, {white, pawn}} || C <- Columns ],
+    Row0 = [{{C, 0}, lists:nth(C + 1, BlackPieces)} || C <- Columns ],
+    Row7 = [{{C, 7}, lists:nth(C + 1, WhitePieces)} || C <- Columns ],
     maps:from_list(Row0 ++ Row1 ++ Row6 ++ Row7).
 
 paint_board(#{panel := Panel,
