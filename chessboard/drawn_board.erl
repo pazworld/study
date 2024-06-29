9ecf5a56826717e430ec135e6c6933e903baaf5b
pazworld <paz@moudamepo.net>
2024-06-29 10:38:27 +0900

    show pieces

--- chessboard/drawn_board.erl
+++ chessboard/drawn_board.erl
@@ -82,11 +82,13 @@ load_images() ->
         {black, bishop} => "black_bishop.png",
         {black, queen}  => "black_queen.png",
         {black, king}   => "black_king.png",
+        {black, pawn}   => "black_pawn.png",
         {white, rook}   => "white_rook.png",
         {white, knight} => "white_knight.png",
         {white, bishop} => "white_bishop.png",
         {white, queen}  => "white_queen.png",
-        {white, king}   => "white_king.png"},
+        {white, king}   => "white_king.png",
+        {white, pawn}   => "white_pawn.png"},
     maps:map(fun(_K, V) -> wxImage:new(
             filename:join("../images", V),
             [{type, ?wxBITMAP_TYPE_PNG}]) end,
@@ -99,7 +101,7 @@ init_board() ->
     WhitePieces = [{white, rook}, {white, knight}, {white, bishop}, {white, queen},
                    {white, king}, {white, bishop}, {white, knight}, {white, rook}],
     Row1 = [{{C, 1}, {white, pawn}} || C <- Columns ],
-    Row6 = [{{C, 1}, {black, pawn}} || C <- Columns ],
+    Row6 = [{{C, 6}, {black, pawn}} || C <- Columns ],
     Row0 = [{{C, 0}, lists:nth(C + 1, WhitePieces)} || C <- Columns ],
     Row7 = [{{C, 7}, lists:nth(C + 1, BlackPieces)} || C <- Columns ],
     maps:from_list(Row0 ++ Row1 ++ Row6 ++ Row7).
@@ -114,14 +116,24 @@ paint_board(#{panel := Panel,
 
     PaintSquare =
         fun(DC, C, R) ->
-            % io:format("~p~n", [square_colour(C, R)]),
             Brush = case square_colour(C, R) of
                 black -> BlackBrush;
                 white -> WhiteBrush
             end,
             Rectangle = rectangle({C, R}, SquareSize),
             wxDC:setBrush(DC, Brush),
-            wxDC:drawRectangle(DC, Rectangle)
+            wxDC:drawRectangle(DC, Rectangle),
+
+            case maps:get({C, R}, Layout, none) of
+                none -> ok;
+                Piece ->
+                    {X, Y, SW, SH} = Rectangle,
+                    Image = wxImage:scale(maps:get(Piece, ImageMap), SW, SH),
+                    PieceBitmap = wxBitmap:new(Image),
+                    wxDC:drawBitmap(DC, PieceBitmap, {X, Y}),
+                    wxImage:destroy(Image),
+                    wxBitmap:destroy(PieceBitmap)
+            end
         end,
 
     DC = wxPaintDC:new(Panel),
